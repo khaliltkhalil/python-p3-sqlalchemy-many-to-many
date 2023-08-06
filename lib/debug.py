246b06d0d2a3ed36920ce646b3cba97356589e4f
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-08-06 14:28:25 -0500

    fix Foreign key issue

--- lib/debug.py
+++ lib/debug.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, delete, select
 from sqlalchemy.orm import sessionmaker
 
-from models import Game, Review, User
+from models import Game, Review, User, game_user
 
-if __name__ == '__main__':
-    
-    engine = create_engine('sqlite:///many_to_many.db')
-    Session = sessionmaker(bind=engine)
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///many_to_many.db", echo=True)
+    Session = sessionmaker(
+        bind=engine,
+    )
     session = Session()
 
-    import ipdb; ipdb.set_trace()
+    session.execute(delete(Game))
+    session.execute(delete(User))
+    session.execute(delete(game_user))
+
+    user_1 = User(name="Ben")
+    user_2 = User(name="Prabhdip")
+    session.add_all([user_1, user_2])
+    session.commit()
+
+    game = Game(title="Super Marvin 128")
+    game.users.append(user_1)
+    game.users.append(user_2)
+    session.add(game)
+    session.commit()
+
+    import ipdb
+
+    ipdb.set_trace()
--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,9 @@
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, event
 from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine import Engine
+import sqlite3
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,11 +13,19 @@ metadata = MetaData(naming_convention=convention)
 Base = declarative_base(metadata=metadata)
 
 
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if type(dbapi_connection) is sqlite3.Connection:  # play well with other DB backends
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+
+
 game_user = Table(
     "game_users",
     Base.metadata,
-    Column("game_id", ForeignKey("games.id"), primary_key=True),
-    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("game_id", ForeignKey("games.id", ondelete="CASCADE"), primary_key=True),
+    Column("user_id", ForeignKey("users.id", ondelete="CASCADE"), primary_key=True),
     extend_existing=True,
 )
 
@@ -48,8 +58,8 @@ class Review(Base):
     score = Column(Integer())
     comment = Column(String())
 
-    game_id = Column(Integer(), ForeignKey("games.id"))
-    user_id = Column(Integer, ForeignKey("users.id"))
+    game_id = Column(Integer(), ForeignKey("games.id", ondelete="CASCADE"))
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
 
     def __repr__(self):
         return (
